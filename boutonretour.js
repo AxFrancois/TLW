e7f3ebef9f187513f23c49116720aefbd37c216b
Axel <axel.francois-charlot@cpe.fr>
2020-12-10 19:01:06 +0100

    correction problème bouton retour haut de page

---
+++ boutonretour.js
@@ -0,0 +1,20 @@
+//------------------------------------BOUTON RETOUR-------------------------------------------------------------------//
+
+jQuery(document).ready(function() {
+    var btn = $('#button');
+
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > 20) {
+        btn.addClass('show');
+        
+      } else {
+        btn.removeClass('show');
+      }
+    });
+    
+    btn.on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({scrollTop:0}, '300');
+    });
+    });
+    
